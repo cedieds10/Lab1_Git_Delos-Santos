d8947dcb6ce38703395cb4c71b36b3b58345fb91
Peter Cedric C. Delos Santos <cedieds10@gmail.com>
2026-02-04 08:15:03 +0800

    Initial commit by Student Delos Santos

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Lab 1: Version Control")
+print("Author:[Peter Cedric C. Delos Santos]")
+print("Student ID: TUPM-25-0068") 
---
+++ wwewew.py
@@ -0,0 +1,2 @@
+mkdir Lab1_Git_Delos Santos
+cd Lab1_Git_Delos Santos 
